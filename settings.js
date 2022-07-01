c28f0285fab15da70ab8f3f5d22d7b3119d29826
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-07-01 19:59:45 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['628979440862','628979440862','628979440862'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['628979440862'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '628979440862' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Zykuan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'AmakawaZKN' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Zykuan' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
+global.youtube = 'there isn't any'
+global.ig = 'there isn't any'
+global.mygc = 'https://chat.whatsapp.com/GFJ62gGdmrQFO1AlvNoGKC'
+global.myweb = 'https://github.com/AmakawaZKN'
 global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = 'AmakawaZKN' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'https://wa.me//+6282133532380' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -62,7 +62,7 @@ global.thumb = fs.readFileSync('./image/chika.jpg')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.BgWelcLeav = 'https://postimg.cc/f3wHVFRz'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
